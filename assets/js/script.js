d262682028f8b78862af8b05ee87da5b1ebf2943
hermawanmangambali <hermawanalimangambali@gmail.com>
2022-01-14 22:30:36 +0700

    Upload all files

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// let getButton = document.querySelector(".btn").addEventListener("click", downloadResume);
+// console.log(getButton);
+
+// function downloadResume(url) {
+//   const a = document.createElement('a')
+//   a.href = url
+//   a.download = url.split('/').pop()
+//   document.body.appendChild(a)
+//   a.click()
+//   document.body.removeChild(a)
+// }
+
+// downloadResume("");
+
+const getImg = document.querySelector(".my-foto");
+console.log(getImg);
+
+// const hammertime = new Hammer(, myOptions);
+// hammertime.on('pan', function(ev) {
+// 	console.log(ev);
+// });
